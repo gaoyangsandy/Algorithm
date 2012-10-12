ffbbb1e68322a67c8cf537d8332c7fec550749ff
username <username@server.com>
2012-10-12 15:37:57 -0700

    matrix search

--- Solution.java
+++ Solution.java
@@ -335,4 +335,39 @@ public class Solution {
 				&& isSameTree(p.right, q.right);
 
 	}
+
+	/*
+	 * Write an efficient algorithm that searches for a value in an m x n
+	 * matrix. This matrix has the following properties:
+	 * 
+	 * Integers in each row are sorted from left to right. The first integer of
+	 * each row is greater than the last integer of the previous row. For
+	 * example,
+	 * 
+	 * Consider the following matrix:
+	 * 
+	 * [ [1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50] ] Given target = 3,
+	 * return true.
+	 */
+	public boolean searchMatrix(int[][] matrix, int target) {
+		int rc = matrix.length;
+		if (rc == 0)
+			return false;
+		int cc = matrix[0].length;
+		if (cc == 0)
+			return false;
+		int l = 0;
+		int r = rc * cc - 1;
+		while (l <= r) {
+			int m = (l + r) / 2;
+			if (matrix[m / cc][m % cc] == target) {
+				return true;
+			} else if (matrix[m / cc][m % cc] > target) {
+				r = m - 1;
+			} else {
+				l = m + 1;
+			}
+		}
+		return false;
+	}
 }
