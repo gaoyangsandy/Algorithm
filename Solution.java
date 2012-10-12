985eb2bc2d4113c626e43cdf32573d3befa9b284
username <username@server.com>
2012-10-12 13:50:30 -0700

    add reverse linked list

--- Solution.java
+++ Solution.java
@@ -161,4 +161,36 @@ public class Solution {
 		}
 		return s.substring(i, j);
 	}
+
+	/**
+	 * Reverse a linked list from position m to n. Do it in-place and in
+	 * one-pass.
+	 * 
+	 * For example: Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+	 * 
+	 * return 1->4->3->2->5->NULL.
+	 * 
+	 * Note: Given m, n satisfy the following condition: 1 <= m <= n <= length of
+	 * list.
+	 */
+	public ListNode reverseBetween(ListNode head, int m, int n) {
+		ListNode p = null;
+		ListNode c = head;
+		for (int i = 0; i < m - 1; i++) {
+			p = c;
+			c = c.next;
+		}
+		for (int i = 0; i < n - m; i++) {
+			ListNode t = c.next;
+			c.next = t.next;
+			if (p != null) {
+				t.next = p.next;
+				p.next = t;
+			} else {
+				t.next = head;
+				head = t;
+			}
+		}
+		return head;
+	}
 }
