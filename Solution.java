0a0de488d9a649152bbae896ce503b0b480855f2
username <username@server.com>
2012-10-16 09:27:58 -0700

    search insert point

--- Solution.java
+++ Solution.java
@@ -715,4 +715,33 @@ public class Solution {
             || search(A,target,mid+1,right);
         }
     }
+
+    /*
+    Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+    You may assume no duplicates in the array.
+    
+    Here are few examples.
+    [1,3,5,6], 5 ? 2
+    [1,3,5,6], 2 ? 1
+    [1,3,5,6], 7 ? 4
+    [1,3,5,6], 0 ? 0
+    */
+    public int searchInsert(int[] A, int target) {
+        return searchInsert(A,target,0,A.length-1);
+    }
+    
+    int searchInsert(int[] A, int target, int left, int right){
+        if(left>right){
+            return left;
+        }
+        
+        int mid = (left+right)/2;
+        if(A[mid]==target) return mid;
+        
+        if(A[mid]>target) return searchInsert(A,target,left,mid-1);
+        
+        return searchInsert(A,target,mid+1,right);
+        
+    }
 }
