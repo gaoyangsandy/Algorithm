f7c0b789b82026d8a93fc726655af4ad00baa454
username <username@server.com>
2012-10-18 08:11:35 -0700

    valid bst

--- Solution.java
+++ Solution.java
@@ -1147,4 +1147,25 @@ public class Solution {
 			return true;
 		return false;
 	}
+
+	/*
+	 * 
+	 * Given a binary tree, determine if it is a valid binary search tree (BST).
+	 * 
+	 * Assume a BST is defined as follows:
+	 * 
+	 * The left subtree of a node contains only nodes with keys less than the
+	 * node's key. The right subtree of a node contains only nodes with keys
+	 * greater than the node's key. Both the left and right subtrees must also
+	 * be binary search trees.
+	 */
+	public boolean isValidBST(TreeNode root) {
+		return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+	public boolean isValidBST(TreeNode root, int lower, int upper) {
+		return root == null || root.val > lower && root.val < upper
+				&& isValidBST(root.left, lower, root.val)
+				&& isValidBST(root.right, root.val, upper);
+	}
 }
