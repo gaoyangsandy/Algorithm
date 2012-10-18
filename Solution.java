3fd81d45e9034c83c462a5706a7c241402f4d4f9
username <username@server.com>
2012-10-17 18:06:35 -0700

    symmetric tree

--- Solution.java
+++ Solution.java
@@ -1039,4 +1039,28 @@ public class Solution {
 		}
 
 	}
+
+	/*
+	 * Given a binary tree, check whether it is a mirror of itself (ie,
+	 * symmetric around its center).
+	 * 
+	 * For example, this binary tree is symmetric:
+	 */
+	public boolean isSymmetric(TreeNode root) {
+		if (root == null)
+			return true;
+		return isMirror(root.left, root.right);
+
+	}
+
+	boolean isMirror(TreeNode root1, TreeNode root2) {
+		if (root1 == null && root2 == null)
+			return true;
+		if (root1 == null || root2 == null)
+			return false;
+
+		return root1.val == root2.val && isMirror(root1.left, root2.right)
+				&& isMirror(root1.right, root2.left);
+
+	}
 }
