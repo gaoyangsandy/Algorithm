e0db1dbcf348499eb250e761ba201ae3000bbaf0
username <username@server.com>
2012-10-19 14:17:02 -0700

    climb stairs

--- Solution.java
+++ Solution.java
@@ -1385,4 +1385,22 @@ public class Solution {
 		}
 	}
 
+	/*
+	 * You are climbing a stair case. It takes n steps to reach to the top.
+	 * 
+	 * Each time you can either climb 1 or 2 steps. In how many distinct ways
+	 * can you climb to the top?
+	 */
+	public int climbStairs(int n) {
+		int s0 = 0;
+		int s1 = 1;
+		int s = 0;
+		for (int i = 0; i < n; i++) {
+			s = s0 + s1;
+			s0 = s1;
+			s1 = s;
+		}
+		return s;
+	}
+
 }
